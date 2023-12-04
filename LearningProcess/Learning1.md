3b70d351edc0bf36e51a8ab26f0ff63a720bac40
Nico <nico.esg.rey@gmail.com>
2023-12-04 19:51:42 +0000

    Creation of the first smart contract for inventory management. -SM written in solidity, using remix IDE

---
+++ LearningProcess/Learning1.md
@@ -0,0 +1,27 @@
+----------------------------------------------Learning from first project---------------------------------------
+
+    1-The basics of Blockchain Technology:
+        1-How to apply it in a real case scenario - INVENTORY MANAGEMENT FOR CLOTH MANUFACTURER
+        2-Main features that make this technology so attractive:
+            a-Traceability
+            b-Real time updates
+            c-Transparency
+            d- No intermediaries
+    3-Main challenges that this technology has to overcome:
+        a-Complexity
+        b-Lack of Talent
+        c-Lack of Standarization
+        d-Energy consumptions
+    4-Cryptographic methods:
+        a-Public and Private Key
+        b-Hash Functions
+        c-Digital Signature
+    5-Smart contracts:
+        a-Definition
+        b-How to build a smart contract using Solidity
+    6-Remix IDE:
+        a-How to use remix IDE to create and test a smart contract
+    7-Ganache:
+        a-Ganache as a personal Blockchain
+        b-How to use Ganache connected with remix IDE in order to test the different features of a smart contract
+------------------------------------------------------------------------------------------------------------------
---
+++ SmartContract_1/InventoryManagement.sol
@@ -0,0 +1,80 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.17;
+
+contract ClothingManufacture {
+    enum Status { Pending, Accepted, Rejected }
+
+    struct Manufacturer {
+        address manufacturerAddress;
+        string manufacturerName;
+    }
+
+    struct Batch {
+        address producer;
+        string sellerName;
+        string companyName;
+        string batchNumber;
+        string quantity;
+        string companyAddress;
+        Status status;
+    }
+
+    Manufacturer public manufacturer;
+    mapping(string => Batch) private batches;
+    mapping(address => string[]) private producerBatches;
+
+    modifier onlyManufacturer() {
+        require(msg.sender == manufacturer.manufacturerAddress, "Only the manufacturer can call this function.");
+        _;
+    }
+
+    constructor(string memory _manufacturerName) {
+        manufacturer.manufacturerAddress = msg.sender;
+        manufacturer.manufacturerName = _manufacturerName;
+    }
+
+
+
+    function createBatch(string memory _batchNumber, address _producer, string memory _sellerName, string memory _companyName, string memory _quantity, string memory _companyAddress) public onlyManufacturer {
+        require(batches[_batchNumber].producer == address(0), "Batch with this number already exists.");
+        Batch storage b = batches[_batchNumber];
+        b.producer = _producer;
+        b.sellerName = _sellerName;
+        b.companyName = _companyName;
+        b.batchNumber = _batchNumber;
+        b.quantity = _quantity;
+        b.companyAddress = _companyAddress;
+        b.status = Status.Pending;
+
+        producerBatches[_producer].push(_batchNumber);
+    }
+
+    function getBatches(address _producer) public view returns (string[] memory) {
+        return producerBatches[_producer];
+    }
+
+    function verifyBatch(string memory _batchNumber) public view returns (address, string memory, string memory, string memory, string memory, string memory, string memory) {
+        Batch memory b = batches[_batchNumber];
+        string memory status;
+        if (b.status == Status.Pending) {
+            status = "Verification pending by the producer";
+        } else if (b.status == Status.Accepted) {
+            status = "Verification accepted by the producer";
+        } else if (b.status == Status.Rejected) {
+            status = "Verification rejected by the producer";
+        }
+        return (b.producer, b.sellerName, b.companyName, b.batchNumber, b.quantity, b.companyAddress, status);
+    }
+
+    function acceptBatch(string memory _batchNumber) public {
+        require(batches[_batchNumber].producer == msg.sender, "Only the producer can accept the batch.");
+        require(batches[_batchNumber].status == Status.Pending, "The batch must be in pending status.");
+        batches[_batchNumber].status = Status.Accepted;
+    }
+
+    function rejectBatch(string memory _batchNumber) public {
+        require(batches[_batchNumber].producer == msg.sender, "Only the producer can reject the batch.");
+        require(batches[_batchNumber].status == Status.Pending, "The batch must be in pending status.");
+        batches[_batchNumber].status = Status.Rejected;
+    }
+}
