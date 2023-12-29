87f4132a5db89f2cea3952b2010430f1539e0bcf
Nico <nico.esg.rey@gmail.com>
2023-12-29 14:05:54 +0000

    Actualization Code explanation

--- Project1_CreationOfBlockchain_Python/Project1Blockchain.py
+++ Project1_CreationOfBlockchain_Python/Project1Blockchain.py
@@ -1,4 +1,7 @@
 #PROJECT1: Simple Blockchain
+import hashlib
+import json
+from time import time
 
 #------------------------------Definition of the Blockchain Class---------------------------------
 class Blockchain:
@@ -31,8 +34,69 @@ class Blockchain:
     #--------------------------------------------------------------------------
 
 
-    #---------------------Definition of NEW_TRANSACTION method-----------------
 
+
+    #--------------------Deffinition of NEW_BLOCK method-----------------------
+    """
+    This method is used to CREATE a NEW BLOCK and ADD it to the BLOCKCHAIN(self.chain)
+    """
+    def new_block(self, proof, previous_hash=None):
+        """
+        1-Parameters:
+        a-proof: Proof that some work has been done
+        b-previous_hash: The hash os the previous Block
+        """
+
+        """
+        The following variable is the creation of a "block" as a Dictionary
+        with their respectives key-value pairs:
+            a-index: The index of the New Block. This is one more than the
+                number of blocks in the currently chain
+            b-timestamp: The current time in which the Block was created
+            c-transactions: The list of transactions that are going to be included 
+                in the new Block
+            d-proof: The proof of work for this Block
+            e-previoous_hash: The Hash of the previous Block
+        """
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+
+        #----------------------------------
+        """
+        This lines reset the List of transactions after adding the information to
+        the new_block dictonary
+        """
+        #----------------------------------
+        """
+        This lines creates add the new block created to the Chain
+        """
+        self.chain.append(block)
+        #----------------------------------
+        """
+        This lines return the new block(dictionary) created
+        """
+        return block
+        #----------------------------------
+        """
+        Overall:
+        This Method:
+            1-Takes in DETAILS of the NEW BLOCK
+            2-Adds that NEW BLOCK to the BLOCKCHAIN
+            3-CLEAR the CURRENT TRANSACTIONS for the NEXT BLOCK
+        """
+    #--------------------------------------------------------------------------
+
+
+
+
+
+
+    #---------------------Definition of NEW_TRANSACTION method-----------------
     def new_transaction(self, sender, recipient, amount):
         """
         -This method creates a new transaction
@@ -62,15 +126,48 @@ class Blockchain:
             3- Indicates that this transaction will be included in the next 
                 block to be added to the blockchain. (Returning the INDEX)
         """
-    def new_transaction(self):
-        # Adds a new transaction to the list of transactions
-        pass
+    #--------------------------------------------------------------------------
 
+
+
+    #------------------Definition of HASH METHOD-------------------------------
+    """
+    This method is used to CREATE a HASH for a BLOCK
+    """
+
+    #-------DECORATOR-------------
+    """
+    -This DECORATOR indicates that this method is a STATIC METHOD which means it belongs
+        to the BLOCKCHAIN CLASS ITSELF
+    -This means you can call the method on the class itself, like this: Blockchain.hash(block),
+        instead of having to create a Blockchain object to call the method.
+    """
     @staticmethod
+    #------Definition of method---
+    """
+    -Definition of the Method
+    -The BLOCK parameter is the BLOCK that we want to HASH
+    """
     def hash(block):
-        # Hashes a Block
-        pass
+    #-----------------------------
+    """
+    -This line converts the block dictionary into a JSON string,
+        sorts it by keys to ensure consistent ordering, and then 
+        encodes it into bytes.
+    -This is necessary because the hashlib.sha256 function requires 
+        a bytes-like object as input.
+    -SIMPLE EXPLANATION: We are telling the function which information use
+        to CREATE the HASH (Everything inside the block and its key-value pairs)
+    """
+        block_string = json.dumps(block, sort_keys=True).encode()
 
+    #-----------------------------
+    """
+    This is where the HASH is CREATED and returned it by the METHOD
+    """
+        return hashlib.sha256(block_string).hexdigest()
+
+    #-----------------------------
     @property
     def last_block(self):
         # Returns the last Block in the chain
---
+++ Project1_CreationOfBlockchain_Python/Project1Blockchain_OnlyCODE.py
@@ -0,0 +1,56 @@
+
+import hashlib
+import json
+from time import time
+
+class Blockchain:
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
+
+
+    def new_transaction(self, sender, recipient, amount):
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount,
+        })
+        return self.last_block['index'] + 1
+
+    def new_block(self, proof, previous_hash=None):
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+        self.current_transactions = []
+        self.chain.append(block)
+        return block
+
+    @staticmethod
+    def hash(block):
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
+
+
+    @property
+    def last_block(self):
+        # Returns the last Block in the chain
+        return self.chain[-1]
+
+
+    def valid_chain(self, chain):
+        last_block = chain[0]
+        current_index = 1
+
+        while current_index < len(chain):
+            block = chain[current_index]
+            if block['previous_hash'] != self.hash(last_block):
+                return False
+
+            last_block = block
+            current_index += 1
+
+        return True
